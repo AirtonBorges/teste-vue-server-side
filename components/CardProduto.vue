01a8ca094b3ecc0df8f6dd7efa04e8ad9332c1c8
Arto <42126588+AirtonBorges@users.noreply.github.com>
2023-08-02 00:36:17 +0000

    fix: arrumado regex

--- components/CardProduto.vue
+++ components/CardProduto.vue
@@ -41,7 +41,7 @@
         preco: { type: Number, required: true },
     })
 
-    const regex = \/d\/(.*)\/view|id=(.*);
+    const regex = /d\/(.*)\/view|id=(.*)/gm;
     const match = regex.exec(props.imagem);
     var xImagemParseada = props.imagem.indexOf("drive") > 1 
       ? "https://drive.google.com/uc?id=" + match?.[1]
